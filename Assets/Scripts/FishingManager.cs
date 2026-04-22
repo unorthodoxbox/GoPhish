a2db4693108bbf90ec8a84d4587176a86312356c
vcd44680 <vcd44680@uga.edu>
2026-04-22 17:22:20 -0400

    recovered all changes...

--- Assets/Scripts/FishingManager.cs
+++ Assets/Scripts/FishingManager.cs
@@ -20,18 +20,12 @@ public class FishingManager : MonoBehaviour
     public GameObject fish, fishingUI;
     public PlayerFishingController player;
 
+    private bool hasWarnedMissingFishingBar;
+    private bool hasWarnedMissingStrengthFill;
+
     private void Awake()
     {
-        if (fishDistanceBarUI == null && fishingUI != null)
-        {
-            Transform barTransform = fishingUI.transform.Find("fishDistanceBar");
-            Transform barTransform2 = fishingUI.transform.Find("strengthFill");
-            if (barTransform != null && barTransform2 != null)
-            {
-                fishDistanceBarUI = barTransform as RectTransform;
-                strengthFillUI = barTransform2 as RectTransform;
-            }
-        }
+        CacheFishingUIRefs();
     }
 
     void Update()
@@ -105,7 +99,9 @@ public class FishingManager : MonoBehaviour
     }
     public void updateUI()
     {
-        if (fishDistanceBarUI == null)
+        CacheFishingUIRefs();
+
+        if (fish == null || player == null)
         {
             return;
         }
@@ -117,11 +113,27 @@ public class FishingManager : MonoBehaviour
         fishEnergyUI.text = "Fish Energy: " + Mathf.RoundToInt(100 * (fish.GetComponent<Fish>().curEnergy / fish.GetComponent<Fish>().maxEnergy)) + "%";
 
         //adjusts fishing bar according to how close the fish is
-        float distance = (20 - fishDistance)/20;
-        fishDistanceBarUI.offsetMax = new Vector2((distance * 385) - 235, fishDistanceBarUI.offsetMax.y);
+        if (fishDistanceBarUI != null)
+        {
+            float distance = (20 - fishDistance) / 20;
+            fishDistanceBarUI.offsetMax = new Vector2((distance * 385) - 235, fishDistanceBarUI.offsetMax.y);
+        }
+        else if (!hasWarnedMissingFishingBar)
+        {
+            Debug.LogWarning("FishingManager could not find a fishing distance bar RectTransform on fishingUI. Text will still update.");
+            hasWarnedMissingFishingBar = true;
+        }
 
         //adjusts strengthBar according to player strength
-        strengthFillUI.offsetMax = new Vector2(strengthFillUI.offsetMax.x, (player.curPullPercent * 255) - 155);
+        if (strengthFillUI != null)
+        {
+            strengthFillUI.offsetMax = new Vector2(strengthFillUI.offsetMax.x, (player.curPullPercent * 255) - 155);
+        }
+        else if (!hasWarnedMissingStrengthFill)
+        {
+            Debug.LogWarning("FishingManager could not find a strength fill RectTransform on fishingUI. Text will still update.");
+            hasWarnedMissingStrengthFill = true;
+        }
     }
     public void checkCatch()
     {
@@ -148,5 +160,44 @@ public class FishingManager : MonoBehaviour
         Destroy(fish);
         isFishing = false;
         fishingUI.SetActive(false);
+    }
+
+    private void CacheFishingUIRefs()
+    {
+        if (fishingUI == null)
+        {
+            return;
+        }
+
+        if (fishDistanceBarUI == null)
+        {
+            fishDistanceBarUI = FindChildRectTransform(fishingUI.transform, "fishDistanceBar")
+                ?? FindChildRectTransform(fishingUI.transform, "fishingBar");
+        }
+
+        if (strengthFillUI == null)
+        {
+            strengthFillUI = FindChildRectTransform(fishingUI.transform, "strengthFill");
+        }
+    }
+
+    private RectTransform FindChildRectTransform(Transform root, string childName)
+    {
+        Transform child = root.Find(childName);
+        if (child != null)
+        {
+            return child as RectTransform;
+        }
+
+        for (int i = 0; i < root.childCount; i++)
+        {
+            RectTransform match = FindChildRectTransform(root.GetChild(i), childName);
+            if (match != null)
+            {
+                return match;
+            }
+        }
+
+        return null;
     }
 }
